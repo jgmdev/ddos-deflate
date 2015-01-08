2967973a1d348b9e4f8fd04d2b1602e0fd284670
jgmdev <jgmdev@gmail.com>
2015-01-07 22:34:04 -0400

    Removed append operators from cron file creation.

--- ddos.sh
+++ ddos.sh
@@ -67,12 +67,12 @@ add_to_cron()
 {
 	rm -f $CRON
 	if [ $FREQ -le 2 ]; then
-		echo "0-59/$FREQ * * * * root /usr/local/ddos/ddos.sh >/dev/null 2>&1" >> $CRON
+		echo "0-59/$FREQ * * * * root /usr/local/ddos/ddos.sh >/dev/null 2>&1" > $CRON
 	else
 		let "START_MINUTE = $RANDOM % ($FREQ - 1)"
 		let "START_MINUTE = $START_MINUTE + 1"
 		let "END_MINUTE = 60 - $FREQ + $START_MINUTE"
-		echo "$START_MINUTE-$END_MINUTE/$FREQ * * * * root /usr/local/sbin/ddos >/dev/null 2>&1" >> $CRON
+		echo "$START_MINUTE-$END_MINUTE/$FREQ * * * * root /usr/local/sbin/ddos >/dev/null 2>&1" > $CRON
 	fi
 	
 	chmod 644 $CRON
