40f02d6145de87bd6830b30a69d2eeb2e6c414c0
jgmdev <jgmdev@gmail.com>
2015-01-09 17:31:55 -0400

    Added support for DESTDIR and a Makefile for streamlined packaging support.

---
+++ Makefile
@@ -0,0 +1,10 @@
+.PHONY: install uninstall
+
+all:
+	@echo "Nothing to compile. Use 'make install' to install (D)Dos Deflate."
+
+install:
+	./install.sh
+
+uninstall:
+	./uninstall.sh
--- install.sh
+++ install.sh
@@ -1,65 +1,71 @@
 #!/bin/bash
-if [ -d '/usr/local/ddos' ]; then
-	echo; echo; echo "Please un-install the previous version first"
+if [ -d "$DESTDIR/usr/local/ddos" ]; then
+	echo "Please un-install the previous version first"
 	exit 0
 else
-	mkdir /usr/local/ddos
+	mkdir -p "$DESTDIR/usr/local/ddos"
 fi
+
 clear
 
-if [ ! -d '/etc/ddos' ]; then
-	mkdir /etc/ddos
+if [ ! -d "$DESTDIR/etc/ddos" ]; then
+	mkdir -p "$DESTDIR/etc/ddos"
 fi
 
 echo; echo 'Installing DOS-Deflate 0.7'; echo
 
-if [ ! -e /etc/ddos/ddos.conf ]; then
+if [ ! -e "$DESTDIR/etc/ddos/ddos.conf" ]; then
 	echo -n 'Adding: /etc/ddos/ddos.conf...'
-	cp config/ddos.conf /etc/ddos/ddos.conf > /dev/null 2>&1
+	cp config/ddos.conf "$DESTDIR/etc/ddos/ddos.conf" > /dev/null 2>&1
 	echo " (done)"
 fi
 
-if [ ! -e /etc/ddos/ignore.ip.list ]; then
+if [ ! -e "$DESTDIR/etc/ddos/ignore.ip.list" ]; then
 	echo -n 'Adding: /etc/ddos/ignore.ip.list...'
-	cp config/ignore.ip.list /etc/ddos/ignore.ip.list > /dev/null 2>&1
+	cp config/ignore.ip.list "$DESTDIR/etc/ddos/ignore.ip.list" > /dev/null 2>&1
 	echo " (done)"
 fi
 
-if [ ! -e /etc/ddos/ignore.host.list ]; then
+if [ ! -e "$DESTDIR/etc/ddos/ignore.host.list" ]; then
 	echo -n 'Adding: /etc/ddos/ignore.host.list...'
-	cp config/ignore.host.list /etc/ddos/ignore.host.list > /dev/null 2>&1
+	cp config/ignore.host.list "$DESTDIR/etc/ddos/ignore.host.list" > /dev/null 2>&1
 	echo " (done)"
 fi
 
 echo -n 'Adding: /usr/local/ddos/LICENSE...'
-cp LICENSE /usr/local/ddos/LICENSE > /dev/null 2>&1
+cp LICENSE "$DESTDIR/usr/local/ddos/LICENSE" > /dev/null 2>&1
 echo " (done)"
 
 echo -n 'Adding: /usr/local/ddos/ddos.sh...'
-cp src/ddos.sh /usr/local/ddos/ddos.sh > /dev/null 2>&1
+cp src/ddos.sh "$DESTDIR/usr/local/ddos/ddos.sh" > /dev/null 2>&1
 chmod 0755 /usr/local/ddos/ddos.sh > /dev/null 2>&1
 echo " (done)"
 
-echo -n 'Creating ddos symlink: /usr/local/sbin/ddos...'
-cp -s /usr/local/ddos/ddos.sh /usr/local/sbin/ddos > /dev/null 2>&1
+echo -n 'Creating ddos script: /usr/local/sbin/ddos...'
+mkdir -p "$DESTDIR/usr/local/sbin/"
+echo "#!/bin/bash" > "$DESTDIR/usr/local/sbin/ddos"
+echo "/usr/local/ddos/ddos.sh \$@" >> "$DESTDIR/usr/local/sbin/ddos"
+chmod 0755 "$DESTDIR/usr/local/sbin/ddos"
 echo " (done)"
 
 echo -n 'Adding man page...'
-cp man/ddos.1 /usr/share/man/man1/ddos.1 > /dev/null 2>&1
-chmod 0644 /usr/share/man/man1/ddos.1 > /dev/null 2>&1
+mkdir -p "$DESTDIR/usr/share/man/man1/"
+cp man/ddos.1 "$DESTDIR/usr/share/man/man1/ddos.1" > /dev/null 2>&1
+chmod 0644 "$DESTDIR/usr/share/man/man1/ddos.1" > /dev/null 2>&1
 echo " (done)"
 
 echo;
 
 if [ -d /etc/init.d ]; then
 	echo -n 'Setting up init script...'
-	cp src/ddos.initd /etc/init.d/ddos > /dev/null 2>&1
-	chmod 0755 /etc/init.d/ddos > /dev/null 2>&1
+	mkdir -p "$DESTDIR/etc/init.d/ddos/"
+	cp src/ddos.initd "$DESTDIR/etc/init.d/ddos" > /dev/null 2>&1
+	chmod 0755 "$DESTDIR/etc/init.d/ddos" > /dev/null 2>&1
 	echo " (done)"
 	
 	# Check if update-rc is installed and activate service
 	UPDATERC_PATH=`whereis update-rc.d`
-	if [ "$UPDATERC_PATH" != "update-rc.d:" ]; then
+	if [ "$UPDATERC_PATH" != "update-rc.d:" ] && [ "$DESTDIR" = "" ]; then
 		echo -n "Activating ddos service..."
 		update-rc.d ddos defaults > /dev/null 2>&1
 		service ddos start > /dev/null 2>&1
@@ -67,19 +73,21 @@ if [ -d /etc/init.d ]; then
 	else
 		echo "ddos service needs to be manually started... (warning)"
 	fi
-elif [ -d /etc/cron.d ]; then
+elif [ -d /etc/cron.d ] && [ "$DESTDIR" = "" ]; then
 	echo -n 'Creating cron to run script every minute...'
+	mkdir -p "$DESTDIR/etc/cron.d/"
 	/usr/local/ddos/ddos.sh --cron > /dev/null 2>&1
 	echo " (done)"
 elif [ -d /usr/lib/systemd/system ]; then
 	echo -n 'Setting up systemd service...'
-	cp src/ddos.service /usr/lib/systemd/system/ > /dev/null 2>&1
-	chmod 0755 /usr/lib/systemd/system/ddos.service > /dev/null 2>&1
+	mkdir -p "$DESTDIR/usr/lib/systemd/system/"
+	cp src/ddos.service "$DESTDIR/usr/lib/systemd/system/" > /dev/null 2>&1
+	chmod 0755 "$DESTDIR/usr/lib/systemd/system/ddos.service" > /dev/null 2>&1
 	echo " (done)"
 	
-	# Check if update-rc is installed and activate service
+	# Check if systemctl is installed and activate service
 	SYSTEMCTL_PATH=`whereis systemctl`
-	if [ "$SYSTEMCTL_PATH" != "systemctl:" ]; then
+	if [ "$SYSTEMCTL_PATH" != "systemctl:" ] && [ "$DESTDIR" = "" ]; then
 		echo -n "Activating ddos service..."
 		systemctl enable ddos > /dev/null 2>&1
 		systemctl start ddos > /dev/null 2>&1
