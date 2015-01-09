aa6c8840bc5e89274fd27bc27f653b7ed430c7e8
jgmdev <jgmdev@gmail.com>
2015-01-09 00:53:58 -0400

    Added daemon mode for faster monitoring of connections.

--- ddos.sh
+++ ddos.sh
@@ -7,9 +7,13 @@
 # The LICENSE file is located in the same directory as this program. Please  #
 #  read the LICENSE file before you make copies or distribute this program   #
 ##############################################################################
+
+CONF_PATH="/usr/local/ddos"
+CONF_PATH="${CONF_PATH}/"
+
 load_conf()
 {
-	CONF="/usr/local/ddos/ddos.conf"
+	CONF="${CONF_PATH}ddos.conf"
 	if [ -f "$CONF" ] && [ ! "$CONF" ==	"" ]; then
 		source $CONF
 	else
@@ -35,10 +39,14 @@ showhelp()
 	echo '-h | --help: Show	this help screen'
 	echo '-c | --cron: Create cron job to run this script regularly (default 1 mins)'
 	echo '-i | --ignore-list: List whitelisted ip addresses.'
+	echo '-d | --start: Initialize a daemon to monitor connections.'
+	echo '-s | --stop: Stop the daemon.'
 	echo '-k | --kill: Block the offending ip making more than N connections'
 }
 
-only_root()
+# Check if super user is executing the 
+# script and exit with message if not.
+su_required()
 {
 	user_id=`id -u`
 	
@@ -52,7 +60,7 @@ only_root()
 # ignore.host.list resolved to ip numbers
 ignore_list()
 {
-	for the_host in $(cat $IGNORE_HOST_LIST | grep -v "#"); do
+	for the_host in $(cat "${CONF_PATH}${IGNORE_HOST_LIST}" | grep -v "#"); do
 		host_ip=`nslookup $the_host | tail -n +3 | grep "Address" | awk '{print $2}'`
 
 		# In case an ip is given instead of hostname
@@ -71,16 +79,18 @@ ignore_list()
 		echo $iface_ip
 	done
     
-    cat $IGNORE_IP_LIST
+    cat "${CONF_PATH}${IGNORE_IP_LIST}"
 }
 
-unbanip()
+ban_ip_list()
 {
 	UNBAN_SCRIPT=`mktemp /tmp/unban.XXXXXXXX`
 	TMP_FILE=`mktemp /tmp/unban.XXXXXXXX`
 	UNBAN_IP_LIST=`mktemp /tmp/unban.XXXXXXXX`
+	
 	echo '#!/bin/sh' > $UNBAN_SCRIPT
 	echo "sleep $BAN_PERIOD" >> $UNBAN_SCRIPT
+	
 	if [ $APF_BAN -eq 1 ]; then
 		while read line; do
 			echo "$APF -u $line" >> $UNBAN_SCRIPT
@@ -92,31 +102,182 @@ unbanip()
 			echo $line >> $UNBAN_IP_LIST
 		done < $BANNED_IP_LIST
 	fi
-	echo "grep -v --file=$UNBAN_IP_LIST $IGNORE_IP_LIST > $TMP_FILE" >> $UNBAN_SCRIPT
-	echo "mv $TMP_FILE $IGNORE_IP_LIST" >> $UNBAN_SCRIPT
+	
+	echo "grep -v --file=$UNBAN_IP_LIST ${CONF_PATH}${IGNORE_IP_LIST} > $TMP_FILE" >> $UNBAN_SCRIPT
+	echo "mv $TMP_FILE ${CONF_PATH}${IGNORE_IP_LIST}" >> $UNBAN_SCRIPT
 	echo "rm -f $UNBAN_SCRIPT" >> $UNBAN_SCRIPT
 	echo "rm -f $UNBAN_IP_LIST" >> $UNBAN_SCRIPT
 	echo "rm -f $TMP_FILE" >> $UNBAN_SCRIPT
+	
+	# Launch script in charge of unbanning after the given period of time
 	. $UNBAN_SCRIPT &
 }
 
 add_to_cron()
 {
+	su_required
+	
 	rm -f $CRON
 	if [ $FREQ -le 2 ]; then
-		echo "0-59/$FREQ * * * * root /usr/local/sbin/ddos >/dev/null 2>&1" > $CRON
+		echo "0-59/$FREQ * * * * root $SBINDIR/ddos >/dev/null 2>&1" > $CRON
 	else
 		let "START_MINUTE = $RANDOM % ($FREQ - 1)"
 		let "START_MINUTE = $START_MINUTE + 1"
 		let "END_MINUTE = 60 - $FREQ + $START_MINUTE"
-		echo "$START_MINUTE-$END_MINUTE/$FREQ * * * * root /usr/local/sbin/ddos >/dev/null 2>&1" > $CRON
+		echo "$START_MINUTE-$END_MINUTE/$FREQ * * * * root $SBINDIR/ddos >/dev/null 2>&1" > $CRON
 	fi
 	
 	chmod 644 $CRON
 }
 
+# Check active connections and ban if neccessary
+check_connections()
+{
+	su_required
+
+	TMP_PREFIX='/tmp/ddos'
+	TMP_FILE="mktemp $TMP_PREFIX.XXXXXXXX"
+	BANNED_IP_MAIL=`$TMP_FILE`
+	BANNED_IP_LIST=`$TMP_FILE`
+	echo "Banned the following ip addresses on `date`" > $BANNED_IP_MAIL
+	echo >>	$BANNED_IP_MAIL
+	BAD_IP_LIST=`$TMP_FILE`
+
+	# Original command to get ip's
+	#netstat -ntu | awk '{print $5}' | cut -d: -f1 | sort | uniq -c | sort -nr > $BAD_IP_LIST
+
+	# Improved command
+	netstat -ntu | \
+		# Strip netstat heading
+		tail -n +3 | \
+		# Match only the given connection states
+		grep -E "$CONN_STATES" | \
+		# Extract only the fifth column
+		awk '{print $5}' | \
+		# Strip port without affecting ipv6 addresses (experimental)
+		sed "s/:[0-9+]*$//g" | \
+		# Sort addresses for uniq to work correctly
+		sort | \
+		# Group same occurrences of ip and prepend amount of occurences found
+		uniq -c | \
+		# Numerical sort in reverse order
+		sort -nr > \
+		$BAD_IP_LIST
+
+	cat $BAD_IP_LIST
+	if [ $KILL -eq 1 ]; then
+		IP_BAN_NOW=0
+		while read line; do
+			CURR_LINE_CONN=$(echo $line | cut -d" " -f1)
+			CURR_LINE_IP=$(echo $line | cut -d" " -f2)
+			if [ $CURR_LINE_CONN -lt $NO_OF_CONNECTIONS ]; then
+				break
+			fi
+			IGNORE_BAN=`ignore_list | grep -c $CURR_LINE_IP`
+			if [ $IGNORE_BAN -ge 1 ]; then
+				continue
+			fi
+			IP_BAN_NOW=1
+			echo "$CURR_LINE_IP with $CURR_LINE_CONN connections" >> $BANNED_IP_MAIL
+			echo $CURR_LINE_IP >> $BANNED_IP_LIST
+			echo $CURR_LINE_IP >> "${CONF_PATH}${IGNORE_IP_LIST}"
+			if [ $APF_BAN -eq 1 ]; then
+				$APF -d $CURR_LINE_IP
+			else
+				$IPT -I INPUT -s $CURR_LINE_IP -j DROP
+			fi
+		done < $BAD_IP_LIST
+		if [ $IP_BAN_NOW -eq 1 ]; then
+			dt=`date`
+			if [ $EMAIL_TO != "" ]; then
+				cat $BANNED_IP_MAIL | mail -s "IP addresses banned on $dt" $EMAIL_TO
+			fi
+			ban_ip_list
+		fi
+	fi
+	rm -f $TMP_PREFIX.*
+}
+
+# Executed as a cleanup function when the daemon is stopped
+on_daemon_exit()
+{
+	if [ -e /var/run/ddos.lock ]; then
+		rm -f /var/run/ddos.lock
+	fi
+}
+
+# Check if daemon us running.
+# Outputs 1 if running 0 if not.
+daemon_running()
+{
+	if [ -e /var/run/ddos.lock ]; then
+		running_pid=$(ps -A | grep ddos | awk '{print $1}')
+		
+		if [ "$running_pid" != "" ]; then
+			current_pid=$(cat /var/run/ddos.lock)
+			
+			if [ "$current_pid" = "$running_pid" ]; then
+				echo "1"
+			fi
+		fi
+	fi
+
+	echo "0"
+}
+
+start_daemon()
+{
+	su_required
+	
+	if [ $(daemon_running) = "1" ]; then
+		echo "ddos daemon is already running..."
+		exit 0
+	fi
+	
+	nohup $0 -l > /dev/null 2>&1 &
+}
+
+stop_daemon()
+{
+	su_required
+	
+	if [ $(daemon_running) = "0" ]; then
+		echo "ddos daemon is not running..."
+		exit 0
+	fi
+	
+	echo "stopping ddos daemon..."
+	
+	pkill ddos
+	
+	if [ -e /var/run/ddos.lock ]; then
+		rm -f /var/run/ddos.lock
+	fi
+}
+
+daemon_loop()
+{
+	su_required
+	
+	if [ $(daemon_running) = "1" ]; then
+		exit 0
+	fi
+	
+	echo "$$" > /var/run/ddos.lock
+	
+	trap 'on_daemon_exit' INT
+	trap 'on_daemon_exit' QUIT
+	trap 'on_daemon_exit' TERM
+	trap 'on_daemon_exit' EXIT
+	
+	while true; do
+		check_connections
+		sleep $DAEMON_FREQ
+	done
+}
 
 load_conf
+
 while [ $1 ]; do
 	case $1 in
 		'-h' | '--help' | '?' )
@@ -124,7 +285,6 @@ while [ $1 ]; do
 			exit
 			;;
 		'--cron' | '-c' )
-			only_root
 			add_to_cron
 			exit
 			;;
@@ -132,6 +292,19 @@ while [ $1 ]; do
 			echo "List of currently whitelisted ip's."
 			echo "==================================="
 			ignore_list
+			exit
+			;;
+		'--start' | '-d' )
+			start_daemon
+			exit
+			;;
+		'--stop' | '-s' )
+			stop_daemon
+			exit
+			;;
+		'--loop' | '-l' )
+			# start daemon loop, used internally by --start | -s
+			daemon_loop
 			exit
 			;;
 		'--kill' | '-k' )
@@ -149,66 +322,4 @@ while [ $1 ]; do
 	shift
 done
 
-only_root
-
-TMP_PREFIX='/tmp/ddos'
-TMP_FILE="mktemp $TMP_PREFIX.XXXXXXXX"
-BANNED_IP_MAIL=`$TMP_FILE`
-BANNED_IP_LIST=`$TMP_FILE`
-echo "Banned the following ip addresses on `date`" > $BANNED_IP_MAIL
-echo >>	$BANNED_IP_MAIL
-BAD_IP_LIST=`$TMP_FILE`
-
-# Original command to get ip's
-#netstat -ntu | awk '{print $5}' | cut -d: -f1 | sort | uniq -c | sort -nr > $BAD_IP_LIST
-
-# Improved command
-netstat -ntu | \
-    # Strip netstat heading
-    tail -n +3 | \
-    # Match only the given connection states
-    grep -E "$CONN_STATES" | \
-    # Extract only the fifth column
-    awk '{print $5}' | \
-    # Strip port without affecting ipv6 addresses (experimental)
-    sed "s/:[0-9+]*$//g" | \
-    # Sort addresses for uniq to work correctly
-    sort | \
-    # Group same occurrences of ip and prepend amount of occurences found
-    uniq -c | \
-    # Numerical sort in reverse order
-    sort -nr > \
-    $BAD_IP_LIST
-
-cat $BAD_IP_LIST
-if [ $KILL -eq 1 ]; then
-	IP_BAN_NOW=0
-	while read line; do
-		CURR_LINE_CONN=$(echo $line | cut -d" " -f1)
-		CURR_LINE_IP=$(echo $line | cut -d" " -f2)
-		if [ $CURR_LINE_CONN -lt $NO_OF_CONNECTIONS ]; then
-			break
-		fi
-		IGNORE_BAN=`ignore_list | grep -c $CURR_LINE_IP`
-		if [ $IGNORE_BAN -ge 1 ]; then
-			continue
-		fi
-		IP_BAN_NOW=1
-		echo "$CURR_LINE_IP with $CURR_LINE_CONN connections" >> $BANNED_IP_MAIL
-		echo $CURR_LINE_IP >> $BANNED_IP_LIST
-		echo $CURR_LINE_IP >> $IGNORE_IP_LIST
-		if [ $APF_BAN -eq 1 ]; then
-			$APF -d $CURR_LINE_IP
-		else
-			$IPT -I INPUT -s $CURR_LINE_IP -j DROP
-		fi
-	done < $BAD_IP_LIST
-	if [ $IP_BAN_NOW -eq 1 ]; then
-		dt=`date`
-		if [ $EMAIL_TO != "" ]; then
-			cat $BANNED_IP_MAIL | mail -s "IP addresses banned on $dt" $EMAIL_TO
-		fi
-		unbanip
-	fi
-fi
-rm -f $TMP_PREFIX.*
+check_connections
