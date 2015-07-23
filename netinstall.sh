6ad5c2355c94d07b915d2c8fb42638a5d0c90b4a
Massimiliano Cuttini <massimiliano.cuttini@phoenixweb.it>
2015-07-23 19:57:29 +0200

    create a net install in order to make massive installations on many server with a single command

---
+++ netinstall.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd ~
+wget https://github.com/phoenixweb/ddos-deflate/archive/master.zip -O ~/ddos-deflate-latest.zip
+unzip ~/ddos-deflate-latest.zip -d ~
+sh ~/ddos-deflate-master/install.sh
