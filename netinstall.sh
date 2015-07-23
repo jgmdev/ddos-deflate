c38fee311dfe76d53dbcd212c65f36fae9c78ea5
Massimiliano Cuttini <massimiliano.cuttini@phoenixweb.it>
2015-07-23 20:24:02 +0200

    fix netinstall paths

--- netinstall.sh
+++ netinstall.sh
@@ -2,5 +2,7 @@
 cd ~
 wget https://github.com/phoenixweb/ddos-deflate/archive/master.zip -O ~/ddos-deflate-latest.zip
 unzip ~/ddos-deflate-latest.zip -d ~
-sh ~/ddos-deflate-master/install.sh
+# install.sh contains relative urls then it's important to CD to the install directory
+cd ~/ddos-deflate-master/
+sh install.sh
 rm -rf ~/ddos-deflate-latest.zip
