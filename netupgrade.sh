ed4230375454b042493304cf7bbe4d766963c672
Massimiliano Cuttini <massimiliano.cuttini@phoenixweb.it>
2015-07-23 21:09:49 +0200

    create a net upgrader in order to make massive installations on many server with a single command

---
+++ netupgrade.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd ~/ddos-deflate-master/
+sh uninstall.sh
+cd ~
+rm -rf ~/ddos-deflate-master/
+curl -s "https://raw.githubusercontent.com/phoenixweb/ddos-deflate/master/netinstall.sh" | bash
