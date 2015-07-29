c0ed9f03c5f76dcab35e54f76e2dcca064626b28
Massimiliano Cuttini <massimiliano.cuttini@phoenixweb.it>
2015-07-29 12:15:51 +0200

    fixed netupgrader

--- netupgrade.sh
+++ netupgrade.sh
@@ -5,14 +5,15 @@ else
 	BRANCH=$1
 fi
 
-if [ -d "$DESTDIR/usr/local/ddos" ]; then
+if [ -d "/usr/local/ddos" ]; then
 	service ddos stop
-	if [ -d "~/ddos-deflate-master/" ]; then
-		mv ~/ddos-deflate-master ~/ddos-deflate
+	if [ -d "/root/ddos-deflate-master/" ]; then
+		rm -rf /root/ddos-deflate
+		mv /root/ddos-deflate-master /root/ddos-deflate
 	fi
-	cd ~/ddos-deflate/
+	cd /root/ddos-deflate/
 	sh uninstall.sh
-	cd ~
-	rm -rf ~/ddos-deflate/
+	rm -rf /root/ddos-deflate/
 fi
+cd ~
 curl -s "https://raw.githubusercontent.com/phoenixweb/ddos-deflate/$BRANCH/netinstall.sh" | bash -s $BRANCH
